11997f16013e9c3665cf007ecead62e1a7b5f605
leticialencar <maria.silvalencars@gmail.com>
2026-04-24 01:17:43 -0300

    chore: update Vite input configuration to include additional JavaScript files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/modal.js',], 
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/alert.js',
+                'resources/js/collection.js',
+                'resources/js/dashboard.js',
+                'resources/js/deleteModal.js',
+                'resources/js/modal.js',
+                'resources/js/password-check.js',
+                'resources/js/rating.js',
+                'resources/js/sort.js',
+            ],
             refresh: true,
         }),
     ],
